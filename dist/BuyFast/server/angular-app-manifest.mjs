5faa8583da6302c2d7d68776b1b723651635ffcf
root <root@luminarilabs.cloud>
2026-04-27 18:23:19 +0000

    full

--- dist/BuyFast/server/angular-app-manifest.mjs
+++ dist/BuyFast/server/angular-app-manifest.mjs
@@ -28,12 +28,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 3505, hash: 'd0e11bd3a3d408316e190f7937561da9c75b20edfd4e2a6308c4732a3effb0da', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 945, hash: 'e895fe3c0aa8222b64d56a6fd5cedd27a81ebf73a5c5cb70bd0c2a4ccb1115d2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'publicar/index.html': {size: 14222, hash: '19d81330473eca2f9477a1381f12f1121666dbdfa35954af3341cbdc9b093e31', text: () => import('./assets-chunks/publicar_index_html.mjs').then(m => m.default)},
-    'disenos/index.html': {size: 9728, hash: 'b421cb8370fa0887e450d8041c268f73f312c6990302fab968300d47d6083977', text: () => import('./assets-chunks/disenos_index_html.mjs').then(m => m.default)},
-    'configurar-producto/index.html': {size: 16013, hash: 'c15e3239294647b631fcc689c388d59c70bfe329f1cd7baf81de02f03dcd430c', text: () => import('./assets-chunks/configurar-producto_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 14466, hash: 'bdf469f3bbe52acb8f749d5a93b3e000ca8ce5abc67e1317105e8ff7ea8d40b3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-KCW5LW4X.css': {size: 9165, hash: 'Gnb6eLZzLCE', text: () => import('./assets-chunks/styles-KCW5LW4X_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 4886, hash: '703ba44526366a518c4551781db79ccce6f2692b822cad2c0ff7aa8f29092b8a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 945, hash: 'a5f6c48c050db2cc21ec899481000e0a3630646fdfe11960af02fd416d4f8bb5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15921, hash: 'd08463fd3c9b23546b487524c2c5c0c54e8f7cbfb6e6ec1752c5e0896859f8e7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'configurar-producto/index.html': {size: 17468, hash: 'c711c015923fdb5e8b36f05f570a3c86f7bbe6b88f8d78bda007dc1b383dfb2f', text: () => import('./assets-chunks/configurar-producto_index_html.mjs').then(m => m.default)},
+    'publicar/index.html': {size: 15677, hash: '23b48ad92d19aa55caaf1736d0cae7d767d22ab1f27e1cc174625c9659d8d227', text: () => import('./assets-chunks/publicar_index_html.mjs').then(m => m.default)},
+    'disenos/index.html': {size: 11211, hash: '8ee73b98db7a91d7064b14f0e4a2fce79f8b79689bb4a7d78ba83d037c08951e', text: () => import('./assets-chunks/disenos_index_html.mjs').then(m => m.default)},
+    'styles-JFLXUV23.css': {size: 11732, hash: '8Qf2OSEsJcE', text: () => import('./assets-chunks/styles-JFLXUV23_css.mjs').then(m => m.default)}
   },
 };
